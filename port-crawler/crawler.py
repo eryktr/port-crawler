74541ee5d306c3d355aa2bbc62a774c673ecdbea
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-24 00:12:16 +0100

    defined the PORTS constant and the main function

--- port-crawler/crawler.py
+++ port-crawler/crawler.py
@@ -1,6 +1,8 @@
 from multiprocessing.dummy import Pool as ThreadPool
 import socket
 
+PORTS = [i for i in range(0, 65535)]
+
 def establish_tcp_connection(server, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -8,3 +10,14 @@ def establish_tcp_connection(server, port):
         return port
     except socket.timeout:
         pass
+
+
+def main(argv):
+    if len(argv) != 3:
+        print("usage: python crawler.py <host> <num_of_threads>")
+        exit(1)
+
+    host = socket.gethostbyname(argv[1])
+    num_threads = int(argv[3])
+
+if __name__ == "__main__":
