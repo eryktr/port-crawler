ac1bb86b49b275e4ea8ddde108890a78d7b0da99
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-24 00:51:44 +0100

    Refactored code

--- port-crawler/crawler.py
+++ port-crawler/crawler.py
@@ -4,31 +4,40 @@ import sys
 
 PORTS = [i for i in range(1, 65535)]
 SERVER = socket.gethostbyname(sys.argv[1])
+OPEN_PORTS = []
+
 
 def establish_tcp_connection(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        print(SERVER, port)
         sock.connect((SERVER, port))
-        return port
-    except socket.timeout:
-        pass
-    except ConnectionRefusedError:
+        OPEN_PORTS.append(port)
+    except OSError:
         pass
 
 
 def main(argv):
-    if len(argv) != 3:
+    def initialize():
+        num_threads = int(argv[2])
+        pool = ThreadPool(num_threads)
+        return pool
+
+    def print_open_ports():
+        print("Open ports: ")
+        for port in OPEN_PORTS:
+            print(port)
+
+    def panic():
         print("usage: python crawler.py <host> <num_of_threads>")
         exit(1)
 
-    num_threads = int(argv[2])
-    pool = ThreadPool(num_threads)
+    if len(argv) != 3:
+        panic()
+    pool = initialize()
     result = pool.map(establish_tcp_connection, PORTS)
+    print_open_ports()
 
-    print("Open ports: ")
-    for port in result:
-        print(port)
+
 
 if __name__ == "__main__":
     main(sys.argv[0:])
