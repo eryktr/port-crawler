552803364aa3077140ca548b0d0c3c35408b6b8c
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-24 14:18:09 +0100

    Refactored code

--- port-crawler/crawler.py
+++ port-crawler/crawler.py
@@ -2,9 +2,22 @@ from multiprocessing.dummy import Pool as ThreadPool
 import socket
 import sys
 
+
+def panic():
+    print("usage: python crawler.py <host> <num_of_threads>")
+    exit(1)
+
+
 PORTS = [i for i in range(1, 65535)]
-SERVER = socket.gethostbyname(sys.argv[1])
 OPEN_PORTS = []
+
+try:
+    SERVER = socket.gethostbyname(sys.argv[1])
+    NUM_THREADS = int(sys.argv[2])
+except IndexError:
+    panic()
+
+POOL = ThreadPool(NUM_THREADS)
 
 
 def establish_tcp_connection(port):
@@ -17,27 +30,16 @@ def establish_tcp_connection(port):
 
 
 def main(argv):
-    def initialize():
-        num_threads = int(argv[2])
-        pool = ThreadPool(num_threads)
-        return pool
 
     def print_open_ports():
         print("Open ports: ")
         for port in OPEN_PORTS:
             print(port)
 
-    def panic():
-        print("usage: python crawler.py <host> <num_of_threads>")
-        exit(1)
-
-    if len(argv) != 3:
-        panic()
-    pool = initialize()
-    result = pool.map(establish_tcp_connection, PORTS)
+    POOL.map(establish_tcp_connection, PORTS)
     print_open_ports()
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[0:])
+    main(sys.argv)
